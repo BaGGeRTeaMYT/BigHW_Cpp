fee3cae287fae825915cf4a87cea1cb41423ea9a
Andrew <andrey.miclanguss@gmail.com>
2024-11-20 00:58:20 +0300

    Test commit message

--- main.cpp
+++ main.cpp
@@ -4,5 +4,6 @@
 int main() {
     // should begin with create table
     OperationCreate tst = OperationCreate(std::string("table_name ({attr1, attr2,attr3}  col1 : int32 = 31, {attr2,attr3}  col2: bytes[12] = 0x1234 )"));
+    // new tests required
     return 0;
 }
--- save_all.ps1
+++ save_all.ps1
@@ -1,3 +1,4 @@
 git add *;
 $commit_message = Read-Host "Enter commit message" 
 git commit -m $commit_message;
+git push
