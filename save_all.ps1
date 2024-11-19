48c5c2f98c997cf53de1b58475f1b35e086b3fbb
Andrew <andrey.miclanguss@gmail.com>
2024-11-20 01:02:18 +0300

    open dota

--- save_all.ps1
+++ save_all.ps1
@@ -1,4 +1,6 @@
 git add *;
 $commit_message = Read-Host "Enter commit message" 
 git commit -m $commit_message;
-git push
+git push;
+Set-Location "C:\Program Files (x86)\Steam\steamapps\common\dota 2 beta\game\bin\win64";
+./dota2.exe
