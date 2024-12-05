0fcd36e9e1651f2d7dbcf4d854f1a4caab626245
Lledely <nemtsov.vovan@yandex.ru>
2024-12-05 22:23:55 +0300

    final commit

--- include/Loader.h
+++ include/Loader.h
@@ -1,21 +1,21 @@
-#ifndef LOADER_H
-#define LOADER_H
+// #ifndef LOADER_H
+// #define LOADER_H
 
-#include <fstream>
-#include <nlohmann/json.hpp>
-#include <string>
+// #include <fstream>
+// #include <nlohmann/json.hpp>
+// #include <string>
 
-class Loader {
-public:
+// class Loader {
+// public:
 
-    Loader( std::string file_name );
+//     Loader( std::string file_name );
 
-    void load_from_file( void );
+//     void load_from_file( void );
     
-private:
+// private:
 
-    std::ifstream m_file;
+//     std::ifstream m_file;
 
-};
+// };
 
-#endif // LOADER_H
+// #endif // LOADER_H
--- include/Saver.h
+++ include/Saver.h
@@ -1,21 +1,21 @@
-#ifndef SAVER_H
-#define SAVER_H
+// #ifndef SAVER_H
+// #define SAVER_H
 
-#include <fstream>
-#include <nlohmann/json.hpp>
-#include <string>
+// #include <fstream>
+// #include <nlohmann/json.hpp>
+// #include <string>
 
-class Saver {
-public:
+// class Saver {
+// public:
 
-    Saver( std::string file_name );
+//     Saver( std::string file_name );
 
-    void save_to_file( void );
+//     void save_to_file( void );
     
-private:
+// private:
 
-    std::ofstream m_file;
+//     std::ofstream m_file;
 
-};
+// };
 
-#endif // SAVER_H
+// #endif // SAVER_H
